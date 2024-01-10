9d5c6893e76c42a6dd4cb1ef42a71e78d6edb503
Abhinav <abhinavsingh12347@gmail.com>
2024-01-11 00:27:00 +0530

    Added explicitely

--- Kotlin Basics/Variables.kt
+++ Kotlin Basics/Variables.kt
@@ -6,5 +6,9 @@ fun main()
     println(name)
     val age = 20 // val is used for constant
     print(age)
-
+    val num:Int = 10 // Explicitly defining the type
+    val decimal:Double = 10.0
+    val flag:Boolean = true
+    val char:Char = 'A'
+    val str:String = "Hello"
 }
