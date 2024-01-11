00447f5061a5f229225969092b195a21e002e537
Abhinav <abhinavsingh12347@gmail.com>
2024-01-11 11:02:50 +0530

    When statement

---
+++ Kotlin Basics/When.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    val animal="Dog"
+    when(animal)
+    {
+        "Cat" -> println("Meow")
+        "Dog" -> println("Bark")
+        "Cow" -> println("Moo")
+        else -> println("I don't know what sound does this animal make")
+    }
+}
