e1777c1171292fe543fb0dc3ea558b4393de8d07
Abhinav <abhinavsingh12347@gmail.com>
2024-01-11 10:39:39 +0530

    Ifelse

---
+++ Kotlin Basics/Ifelse.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    val age = 20
+    if(age>18)
+    {
+        println("You are eligible to vote")
+    }
+    else
+    {
+        println("You are not eligible to vote")
+    }
+}
